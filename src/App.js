f46f7e47761d78f58725df7921dbba9f62b46bc8
reactionic127 <richard.gill127@gmail.com>
2020-08-05 16:22:34 +0800

    updated version: 0.1.4

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     internalReferenceId: Math.floor(Math.random() * 10000),
     salespersonName: 'Li Jin',
     projectId: 'random project id',
-    jwtToken: 'random token'
+    jwtToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
   }
   return (
     <div className="App">
--- src/components/DesignTool.js
+++ src/components/DesignTool.js
@@ -1,23 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './DesignTool.css';
 
 const DesignTool = (props) => {
   const { internalReferenceId, salespersonName, projectId, jwtToken } = props;
+  const [newToken, setNewToken] = useState('');
+
+  const generateNewToken = () => {
+    var token           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 156; i++ ) {
+      token += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    setNewToken(token);
+  }
+
   return (
     <div className="container">
       <div className="values">
-        <p>
-          Internal Reference Id: {internalReferenceId}
-        </p>
-        <p>
-          Sales Person Name: {salespersonName}
-        </p>
-        <p>
-          Project Id: {projectId}
-        </p>
-        <p>
-          JWT Token: {jwtToken}
-        </p>
+        <div className="item">
+          <div>
+            Internal Reference Id:
+          </div>
+          <div>
+            {internalReferenceId}
+          </div>
+        </div>
+        <div className="item">
+          <div>
+            Sales Person Name:
+          </div>
+          <div>
+            {salespersonName}
+          </div>
+        </div>
+        <div className="item">
+          <div>
+            Project Id:
+          </div>
+          <div>
+            {projectId}
+          </div>
+        </div>
+        <div className="item">
+          <div>
+            JWT Token:
+          </div>
+          <div>
+            {newToken || jwtToken}
+          </div>
+        </div>
+        <div className="item">
+          <button onClick={generateNewToken}>
+            Request New Auth Token
+          </button>
+        </div>
       </div>
     </div>
   );
