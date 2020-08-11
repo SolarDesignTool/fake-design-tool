f4bca58f4b985397d3516812d848e5517ee5a9e8
reactionic127 <richard.gill127@gmail.com>
2020-08-11 09:56:30 +0800

    added fake design tool component

---
+++ src/components/FakeDesignTool.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+// import './FakeDesignTool.css';
+
+const FakeDesignTool = (props: any) => {
+  const { internalReferenceId, salespersonName, projectId, jwtToken, generateNewTokenCallback } = props;
+  return (
+    <div className="container">
+      <div className="values">
+        <div className="item">
+          <div>
+            Internal Reference Id:
+          </div>
+          <div>
+            {internalReferenceId}
+          </div>
+        </div>
+        <div className="item">
+          <div>
+            Sales Person Name:
+          </div>
+          <div>
+            {salespersonName}
+          </div>
+        </div>
+        <div className="item">
+          <div>
+            Project Id:
+          </div>
+          <div>
+            {projectId}
+          </div>
+        </div>
+        <div className="item">
+          <div>
+            JWT Token:
+          </div>
+          <div>
+            {jwtToken}
+          </div>
+        </div>
+        <div className="item">
+          <button onClick={generateNewTokenCallback}>
+            Request New Auth Token
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { FakeDesignTool };
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { FakeDesignTool } from './components/FakeDesignTool'
 import './index.css';
 
-class App extends React.Component{
-  render(){
-    return(
-      <div>Hello World</div>
-    )
+function App() {
+  const [newToken, setNewToken] = useState('');
+
+  const parameters = {
+    internalReferenceId: Math.floor(Math.random() * 10000),
+    salespersonName: 'Li Jin',
+    projectId: 'random project id',
+    jwtToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
   }
+
+  const generateNewToken = () => {
+    var token           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 156; i++ ) {
+      token += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    setNewToken(token);
+  }
+
+  return (
+    <div className="App">
+      <FakeDesignTool
+        internalReferenceId={parameters.internalReferenceId}
+        salespersonName={parameters.salespersonName}
+        projectId={parameters.projectId}
+        jwtToken={newToken || parameters.jwtToken}
+        generateNewTokenCallback={generateNewToken}
+      />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
