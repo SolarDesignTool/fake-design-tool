7cf0369c9c5543bc8e5e3f4cb09327312fd8c2fd
reactionic127 <richard.gill127@gmail.com>
2020-08-11 17:36:59 +0800

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { ReactElement, useState } from 'react';
 import ReactDOM from 'react-dom';
-import { FakeDesignTool } from './components/FakeDesignTool'
+import { FakeDesignTool } from './components/FakeDesignTool';
 import './index.css';
 
-function App() {
+function App(): ReactElement {
   const [newToken, setNewToken] = useState('');
 
   const parameters = {
@@ -11,17 +11,17 @@ function App() {
     salespersonName: 'Li Jin',
     projectId: 'random project id',
     jwtToken: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c'
-  }
+  };
 
-  const generateNewToken = () => {
-    var token           = '';
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < 156; i++ ) {
+  const generateNewToken = (): void => {
+    let token           = '';
+    const characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    for (let i = 0; i < 156; i++) {
       token += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
     setNewToken(token);
-  }
+  };
 
   return (
     <div className="App">
@@ -36,4 +36,4 @@ function App() {
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App />, document.getElementById('app'));
